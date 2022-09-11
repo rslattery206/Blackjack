cd592450a80e96e1a6c31612821ff2d353670f30
Ryan <rlslatte@purdue.edu>
2022-09-12 01:28:51 +0800

    Round can be reset

--- main.py
+++ main.py
@@ -4,13 +4,36 @@ import game
 
 game.initializeGame()
 deck = game.deck
-
+money = 100
 my_hand = []
 dealer_hand = []
 my_hand.append(game.drawCard())
 dealer_hand.append(game.drawCard())
 my_hand.append(game.drawCard())
 dealer_hand.append(game.drawCard())
+
+
+def newRound():
+    # Handles the creation of a new round
+    my_hand.clear()
+    dealer_hand.clear()
+    my_hand.append(game.drawCard())
+    dealer_hand.append(game.drawCard())
+    my_hand.append(game.drawCard())
+    dealer_hand.append(game.drawCard())
+    # Update Cards
+    window["0_PLAYER_CARD"].update(filename="images/{}.png".format(my_hand[0].id))
+    window["1_PLAYER_CARD"].update(filename="images/{}.png".format(my_hand[1].id))
+    window["2_PLAYER_CARD"].update(filename="images/blank.png")
+    window["3_PLAYER_CARD"].update(filename="images/blank.png")
+    window["4_PLAYER_CARD"].update(filename="images/blank.png")
+    window["0_DEALER_CARD"].update(filename="images/{}.png".format(dealer_hand[0].id))
+    window["1_DEALER_CARD"].update(filename="images/hidden_card.png")
+    # Update Totals
+    window["_PLAYER_TOTAL"].update(getTotal(my_hand))
+    window["_DEALER_TOTAL"].update(dealer_hand[0].val)
+    # Make Buttons Clickable
+    window["_PLAYER_DRAW"].update(disabled=False)
 
 
 def getTotal(hand):
@@ -20,25 +43,41 @@ def getTotal(hand):
     return total
 
 
+def hit():
+    my_hand.append(game.drawCard())
+    if (getTotal(my_hand)) > 21:  # bust
+        endTurn()
+
+
+def endTurn():
+    # Handles end of player action
+    window["_PLAYER_DRAW"].update(disabled=True)
+    revealDealerHoleCard()
+
+
+def revealDealerHoleCard():
+    window["1_DEALER_CARD"].update(filename="images/{}.png".format(dealer_hand[1].id))
+
+
 top_bar = [
     sg.Text("Blackjack 2022", font=("SEC Bengali", 20)),
-    sg.Column([[sg.Image(filename="images/icon.png", tooltip="Logo styled using MS Paint")]], justification='right')
+    sg.Column([[sg.Image(filename="images/icon.png", tooltip="Logo styled using MS Paint")]], justification='right'),
+    [sg.Text("$" + str(money))],
 ]
 
 dealer_side = [
     [sg.Text("The Dealer")],
-    [sg.Text(getTotal(dealer_hand))],
+    [sg.Text(dealer_hand[0].val, key="_DEALER_TOTAL")],
     [sg.Image(filename="images/{}.png".format(dealer_hand[0].id), key="0_DEALER_CARD"),
      sg.Image(filename="images/hidden_card.png", key="1_DEALER_CARD"),
      sg.Image(filename="images/blank.png", key="2_DEALER_CARD"),
      sg.Image(filename="images/blank.png", key="3_DEALER_CARD"),
      sg.Image(filename="images/blank.png", key="4_DEALER_CARD")]
 ]
-#    [sg.Image(filename="images/{}.png".format(card.id), key='420') for card in my_hand]
 
 player_side = sg.Column([
     [sg.Text("Player")],
-    [sg.Text(getTotal(my_hand), key="PLAYER_TOTAL")],
+    [sg.Text(getTotal(my_hand), key="_PLAYER_TOTAL")],
     [sg.Image(filename="images/{}.png".format(my_hand[0].id), key="0_PLAYER_CARD"),
      sg.Image(filename="images/{}.png".format(my_hand[1].id), key="1_PLAYER_CARD"),
      sg.Image(filename="images/blank.png", key="2_PLAYER_CARD"),
@@ -50,18 +89,25 @@ layout = [
     top_bar,
     [dealer_side],
     [player_side],
-    [sg.Button(button_text="Add Card", key="_HIT")],
+    # Buttons
+    [sg.Button(button_text="Hit", key="_PLAYER_DRAW"),
+     sg.Button(button_text="Stand", key="_STAND")],
+    [sg.Button(button_text="manual_end_round", key="manual_end_round")]
 ]
 
-window = sg.Window(title="durrr", layout=layout, size=(400, 500))
+window = sg.Window(title="durrr", layout=layout, size=(400, 550))
+
 while True:
     event, values = window.read()
-    if event == "_HIT":
-        #HIT
-        my_hand.append(game.drawCard())
-        window["PLAYER_TOTAL"].update(getTotal(my_hand))
+    if event == "_PLAYER_DRAW":
+        hit()
+        window["_PLAYER_TOTAL"].update(getTotal(my_hand))
         new_card_index = len(my_hand) - 1
         window[str(new_card_index) + "_PLAYER_CARD"].update(filename="images/{}.png".format(my_hand[new_card_index].id))
+    if event == "_STAND":
+        endTurn()
+    if event == "manual_end_round":
+        newRound()
     elif event == sg.WIN_CLOSED:
         break
 
